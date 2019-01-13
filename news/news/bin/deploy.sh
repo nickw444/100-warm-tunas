29f43c8ae4059e10e248b30fcfb5f87650a8f783
Travis CI <travis@travis-ci.org>
2019-01-13 11:32:04 +0000

    news: Build for 319cdd9c9d656f1ffd8996c1af637a2da47ca8c2: Show reasonable message

---
+++ news/news/bin/deploy.sh
@@ -0,0 +1,15 @@
+set -eu
+
+main() {
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "Travis CI"
+    git clone https://${GITHUB_TOKEN}@github.com/nickw444/100-warm-tunas.git
+    cp -r _site/news/ 100-warm-tunas/news/
+
+    cd 100-warm-tunas
+    git add news/
+    git commit -m "news: Build for ${TRAVIS_COMMIT}: ${TRAVIS_COMMIT_MESSAGE}"
+    git push origin master
+}
+
+main
